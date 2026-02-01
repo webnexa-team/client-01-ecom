4bef8215428b5dd9dcaac305ad2bd60f89064fa2
Arunmaity2003 <arunmaity0610@gmail.com>
2026-02-01 17:27:40 +0530

    Commit by arun da

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
     <div>
       <h1>this is the home section</h1>
       <p>commit by samir</p>
+      <p>this is the end hold your breath and count to 10 | by Arun</p>
     </div>
   )
 }
